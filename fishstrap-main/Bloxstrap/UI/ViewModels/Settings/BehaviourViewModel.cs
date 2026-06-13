7a65933a689b57fb54b8ea35333eb1278663515b
Jovan1-maker <negneg1231231232gmail.com>
2026-06-13 13:25:04 +0800

    Initial commit

---
+++ fishstrap-main/Bloxstrap/UI/ViewModels/Settings/BehaviourViewModel.cs
@@ -0,0 +1,155 @@
+﻿using Bloxstrap.AppData;
+using Bloxstrap.Enums;
+using Bloxstrap.Integrations;
+using Bloxstrap.RobloxInterfaces;
+using Wpf.Ui.Common.Interfaces;
+
+namespace Bloxstrap.UI.ViewModels.Settings
+{
+    public class BehaviourViewModel : NotifyPropertyChangedViewModel, INavigationAware
+    {
+
+        public BehaviourViewModel()
+        {
+            App.Cookies.StateChanged += (object? _, CookieState state) => CookieLoadingFailed = state != CookieState.Success && state != CookieState.Unknown;
+        }
+
+        public void OnNavigatedTo() 
+        {
+            OnPropertyChanged(nameof(VulkanFullscreenAllowed));
+            OnPropertyChanged(nameof(EnableFakeBorderlessFullscreen));
+        }
+
+        public void OnNavigatedFrom() { } // has to be here because of INavigationAware, we will just leave it empty
+
+        public bool IsRobloxInstallationMissing => String.IsNullOrEmpty(App.RobloxState.Prop.Player.VersionGuid) && String.IsNullOrEmpty(App.RobloxState.Prop.Studio.VersionGuid);
+        public bool VulkanFullscreenAllowed => WindowManipulation.WindowManipulationAvailable && (App.FastFlags.GetPreset("Rendering.Mode.Vulkan") ?? "False").Equals("True", StringComparison.OrdinalIgnoreCase);
+
+        public bool CookieAccess
+        {
+            get => App.Settings.Prop.AllowCookieAccess;
+            set
+            {
+                App.Settings.Prop.AllowCookieAccess = value;
+                if (value)
+                    Task.Run(App.Cookies.LoadCookies);
+
+                OnPropertyChanged(nameof(CookieAccess));
+            }
+        }
+
+        // guh
+        private bool _cookieLoadingFailed;
+        public bool CookieLoadingFailed
+        {
+            get => _cookieLoadingFailed;
+            set
+            {
+                _cookieLoadingFailed = value;
+                OnPropertyChanged(nameof(CookieLoadingFailed));
+            }
+        }
+
+        public bool EnableBetterMatchmaking
+        {
+            get => App.Settings.Prop.EnableBetterMatchmaking;
+            set => App.Settings.Prop.EnableBetterMatchmaking = value;
+        }
+
+        public bool EnableBetterMatchmakingRandomization
+        {
+            get => App.Settings.Prop.EnableBetterMatchmakingRandomization;
+            set => App.Settings.Prop.EnableBetterMatchmakingRandomization = value;
+        }
+        public bool EnableFakeBorderlessFullscreen
+        {
+            get => App.Settings.Prop.FakeBorderlessFullscreen;
+            set => App.Settings.Prop.FakeBorderlessFullscreen = value;
+        }
+
+        public bool ConfirmLaunches
+        {
+            get => App.Settings.Prop.ConfirmLaunches;
+            set => App.Settings.Prop.ConfirmLaunches = value;
+        }
+
+        public bool ForceRobloxLanguage
+        {
+            get => App.Settings.Prop.ForceRobloxLanguage;
+            set => App.Settings.Prop.ForceRobloxLanguage = value;
+        }
+
+        public bool BackgroundUpdates
+        {
+            get => App.Settings.Prop.BackgroundUpdatesEnabled;
+            set => App.Settings.Prop.BackgroundUpdatesEnabled = value;
+        }
+
+        public CleanerOptions SelectedCleanUpMode
+        {
+            get => App.Settings.Prop.CleanerOptions;
+            set => App.Settings.Prop.CleanerOptions = value;
+        }
+
+        public IEnumerable<CleanerOptions> CleanerOptions { get; } = CleanerOptionsEx.Selections;
+
+        public CleanerOptions CleanerOption
+        {
+            get => App.Settings.Prop.CleanerOptions;
+            set
+            {
+                App.Settings.Prop.CleanerOptions = value;
+            }
+        }
+
+        private List<string> CleanerItems = App.Settings.Prop.CleanerDirectories;
+
+        public bool CleanerLogs
+        {
+            get => CleanerItems.Contains("RobloxLogs");
+            set
+            {
+                if (value)
+                    CleanerItems.Add("RobloxLogs");
+                else
+                    CleanerItems.Remove("RobloxLogs"); // should we try catch it?
+            }
+        }
+
+        public bool CleanerCache
+        {
+            get => CleanerItems.Contains("RobloxCache");
+            set
+            {
+                if (value)
+                    CleanerItems.Add("RobloxCache");
+                else
+                    CleanerItems.Remove("RobloxCache");
+            }
+        }
+
+        public bool CleanerStudioCache
+        {
+            get => CleanerItems.Contains("RobloxStudioCache");
+            set
+            {
+                if (value)
+                    CleanerItems.Add("RobloxStudioCache");
+                else
+                    CleanerItems.Remove("RobloxStudioCache");
+            }
+        }
+
+        public bool CleanerFishstrap
+        {
+            get => CleanerItems.Contains("FishstrapLogs");
+            set
+            {
+                if (value)
+                    CleanerItems.Add("FishstrapLogs");
+                else
+                    CleanerItems.Remove("FishstrapLogs");
+            }
+        }
+    }
+}
