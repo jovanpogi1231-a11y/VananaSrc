d6d3a2dd7f0400ef0d01fb36f7a5c1fd4c7069bd
Skil Dkl <briannaell.i.s6047@gmail.com>
2026-06-13 05:29:00 +0000

    Rename Fishstrap icon resources to VanStrap

--- fishstrap-main/Bloxstrap/Enums/BootstrapperIcon.cs
+++ fishstrap-main/Bloxstrap/Enums/BootstrapperIcon.cs
@@ -2,9 +2,9 @@
 {
     public enum BootstrapperIcon
     {
-        [EnumName(StaticName = "Fishstrap")] //ermmmm it goes unused as of rn
-        IconFishstrap,
-        [EnumName(StaticName = "Fishstrap")]
+        [EnumName(StaticName = "VanStrap")]
+        IconVanStrap,
+        [EnumName(StaticName = "VanStrap")]
         IconBloxstrap,
         [EnumName(StaticName = "2008")]
         Icon2008,
