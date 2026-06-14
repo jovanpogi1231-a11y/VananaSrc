d33c7a21ed237559548ff604665231225220251a
Tuting Po <kirkgutier.re.z.2036@gmail.com>
2026-06-14 03:44:17 +0000

    Fix CS0111 duplicate BehaviourPage class and show added accounts after WebView login

--- fishstrap-main/Bloxstrap/UI/Elements/Settings/Pages/BootstrapperPage.xaml.cs
+++ fishstrap-main/Bloxstrap/UI/Elements/Settings/Pages/BootstrapperPage.xaml.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 using Bloxstrap.UI.ViewModels.Settings;
 
 namespace Bloxstrap.UI.Elements.Settings.Pages
 {
     /// <summary>
-    /// Interaction logic for BehaviourPage.xaml
+    /// Interaction logic for BootstrapperPage.xaml
     /// </summary>
-    public partial class BehaviourPage
+    public partial class BootstrapperPage
     {
-        public BehaviourPage()
+        public BootstrapperPage()
         {
             DataContext = new BehaviourViewModel();
             InitializeComponent();
